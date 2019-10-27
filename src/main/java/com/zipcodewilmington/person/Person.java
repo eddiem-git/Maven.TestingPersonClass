51eb1315654bff0a91fb64802b9e414187cc8063
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-27 18:22:05 -0400

    TestingPerson

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,77 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String eyeColor;
+    private Integer height;
+    private Double weight;
+    private String hairColor;
+
+
+
 
     public Person() {
+        this.age = 5;
+        this.weight = 72.50;
+        this.height = 72;
+        this.eyeColor = "brown";
+        this.hairColor = "Black";
+        this.name = "John";
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+    this.name = name;
+
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+    public void setEyeColor(String eyeColor){
+        this.eyeColor = eyeColor;
+    }
+    public void setHeight(Integer height){
+        this.height = height;
+
+    }
+    public void setWeight(Double weight){
+        this.weight = weight;
+
+    }
+    public void setHairColor(String hairColor){
+        this.hairColor = hairColor;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
+
+    public String getEyeColor() {
+        return eyeColor; }
+
+    public Integer getHeight() {
+        return height; }
+
+    public Double getWeight() {
+        return weight; }
+
+    public String getHairColor() {
+        return hairColor; }
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -10,8 +10,11 @@ public class TestPerson {
     @Test
     public void testDefaultConstructor() {
         // Given
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
+        String expectedName = "John";
+        Integer expectedAge = 5;
+        String expectedHairColor = "Black";
+        Double expectedWeight = 72.50;
+        Integer expectedHeight = 72;
 
         // When
         Person person = new Person();
@@ -19,15 +22,23 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        String actualHair = person.getHairColor();
+        Integer actualHeight = person.getHeight();
+        Double actualWeight = person.getWeight();
+
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedHairColor, actualHair);
+        Assert.assertEquals(expectedWeight, actualWeight);
+        Assert.assertEquals(expectedHeight, actualHeight);
+
     }
 
     @Test
     public void testConstructorWithName() {
         // Given
-        String expected = "Leon";
+        String expected = "John";
 
         // When
         Person person = new Person(expected);
@@ -95,4 +106,54 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void testSetHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Black";
+
+        // When
+        person.setHairColor(expected);
+
+        // Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testEyeColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Brown";
+
+        // When
+        person.setEyeColor(expected);
+        // Then
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetWeight() {
+        // Given
+        Person person = new Person();
+        Double expected = 72.50;
+
+        // When
+        person.setWeight(expected);
+        // Then
+        Double actual = person.getWeight();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 72;
+
+        // When
+        person.setHeight(expected);
+        // Then
+        Integer actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
